73dc96a56d7b45bbe15049a397e508c08126d8c8
barvolar <86926525+barvolar@users.noreply.github.com>
2022-04-03 19:09:09 +0300

    03.04.2022//19.09

--- HP/Assets/Health.cs
+++ HP/Assets/Health.cs
@@ -7,12 +7,23 @@ namespace HealthUI
         private float _powerDamage = 10f;
         private float _powerHealing = 7f;
         private float _maxValueLife = 100f;     
+
         public float CurrentValueLife { get; private set; }
 
         private void Update()
         {
             ControlExitBorder();
         }
+
+        private void ControlExitBorder()
+        {
+            if (CurrentValueLife < 0)
+                CurrentValueLife = 0;
+
+            if (CurrentValueLife > _maxValueLife)
+                CurrentValueLife = _maxValueLife;
+        }
+
         public void TakeDamae()
         {
             if (CurrentValueLife > 0)
@@ -23,15 +34,6 @@ namespace HealthUI
         {
             if (CurrentValueLife < 100)
                 CurrentValueLife += _powerHealing;
-        }
-
-        private void ControlExitBorder()
-        {
-            if (CurrentValueLife < 0)
-                CurrentValueLife = 0;
-
-            if (CurrentValueLife > _maxValueLife)
-                CurrentValueLife = _maxValueLife;
         }
     }
 }
